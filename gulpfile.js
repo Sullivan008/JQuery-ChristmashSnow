36d050da28cb7f61c73aa315439ed3d2beac45c2
Sullivan008 <kozakpeter0424@gmail.com>
2019-11-21 22:08:22 +0100

    Node Modules Installion; JQuery- JQuery SnowFall- Boostrap Instllion; Gulp File Initialization and Generate css and js files.

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var elixir = require('laravel-elixir');
+var gulp = require('gulp');
+
+elixir.config.sourcemaps = false;
+
+elixir(function(mix){
+    mix.sass('resources/assets/sass/app.scss', 'resources/assets/css');
+
+    mix.styles(
+        [
+            'css/app.css'
+        ], 
+        'public/css/Site.css',                                          
+        'resources/assets');
+
+    var bowerPath = 'bower/vendor';
+
+    mix.scripts(
+        [
+            bowerPath + '/jquery/dist/jquery.min.js',
+            bowerPath + '/jquery-snowfall/dist/snowfall.jquery.min',
+            bowerPath + '/bootstrap/dist/js/bootstrap.min.js',
+
+            'js/*.js'
+        ],
+        'public/js/site.js',
+        'resources/assets');
+});
